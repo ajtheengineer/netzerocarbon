13a50a56a41623e4899f7558a163d052d1e08c0d
ajtheengineer <ajtheengineer@gmail.com>
2022-07-03 13:42:54 -0700

    Update icons and title

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,6 +3,6 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'True lifetime CO2 emissions')
+    cy.contains('h1', 'Lifetime CO2 emissions')
   })
 })
--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/carbon-footprint.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="True lifetime CO2 emissions" />
+      <HelloWorld msg="Lifetime CO2 emissions" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
