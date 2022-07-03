982018c1d625488bd8924b12bcb9717d8435212d
ajtheengineer <ajtheengineer@gmail.com>
2022-07-03 14:35:24 -0700

    Add search with limited data and styling

---
+++ src/components/CarSearch.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+let input = ref("");
+const cars = ["Prius Prime", "Prius", "Tesla", "Rav 4 Prime"];
+function filteredList() {
+  return cars.filter((car) =>
+    car.toLowerCase().includes(input.value.toLowerCase())
+  );
+}
+</script>
+
+<template>
+  <input type="text" v-model="input" placeholder="Search cars..." />
+  <div class="item car" v-for="car in filteredList()" :key="car">
+    <p>{{ car }}</p>
+  </div>
+  <div class="item error" v-if="input&&!filteredList().length">
+     <p>No results found!</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '@/components/TheWelcome.vue'
+import CarSearch from '@/components/CarSearch.vue'
 
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <CarSearch />
   </main>
-
 </template>
